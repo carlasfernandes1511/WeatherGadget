cd3ce816bd21f22e6575952de16b1f25add01f6a
carlasfernandes1511 <carla.sfernandes@outlook.com>
2023-04-01 17:46:27 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,41 @@
+
+
+// VARIAVEIS = um espaco da memória do computador que guardamos algo (número, letra, texto, imagem)
+// FUNCAO = um trecho de código que só é executado quando é chamado
+
+let chave = "7d9394490c5b538f307f4fef0baa63c6"
+
+
+function colocaNaTela (dados){
+
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".descricao").innerHTML = dados.weather[0].description
+   // document.querySelector(".umidade").innerHTML = 
+    document.querySelector(".icone").src= "https://openweathermap.org/img/wn/" + dados.weather
+    [0].icon + ".png"
+}
+
+async function buscarCidade(cidade){
+    let dados = await  fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" + 
+        cidade +
+        "&appid=" + 
+        chave + 
+        "&lang=pt_br" + 
+        "&units=metric") 
+        .then ( resposta => resposta.json())
+
+       colocaNaTela(dados)
+        
+        // AWAIT - esperar a informacao para acessar o servidor
+
+    }
+
+function cliqueiNoBotao(){
+
+    let cidade = document.querySelector(".inputCidade").value
+    buscarCidade(cidade)
+}
+
